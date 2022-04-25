acb32f062565e0d586b1c007920757b620aaf0fe
Emir Elkabes <desperad5@yahoo.com>
2022-04-25 15:09:24 +0300

    Basic Component usage

---
+++ src/components/GoalList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './GoalList.css'
+const GoalList = () => {
+    return (
+    <ul className="goal-list">
+      <li>Finish the course</li>
+      <li>Learn about the course main topic</li>
+      <li>Help other students in the course</li>
+    </ul>
+    );
+}
+export default GoalList;
