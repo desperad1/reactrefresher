4fe68565ac3d551a439c59057daf406dce2c9282
Emir Elkabes <desperad5@yahoo.com>
2022-04-25 15:49:14 +0300

    Basic Communication between parent and child components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 const App = () => {
+  const courseGoals=[
+    {id:'cg1',text:'Finish the course'},
+    {id:'cg2',text:'Learn about the course main topic'},
+    {id:'cg3',text:'Help other students in the course'},
+];
+const onAddGoalHandler =(goal)=>{
+  courseGoals.push(goal);
+  console.log(courseGoals);
+}
   return <div className="course-goals">
     <h2>Course Goals</h2>
-    <GoalList/>
+    <NewGoal onAddGoal={onAddGoalHandler}/>
+    <GoalList goals={courseGoals}/>
   </div>
   ;
 };
